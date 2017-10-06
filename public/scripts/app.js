d6f25e0facfcdd8b813113b069fc75d94beb4456
olivaluca85@gmail.com <olivaluca85@gmail.com>
2017-10-06 18:39:27 +0200

    First React app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,11 @@
 console.log('App.js is running!');
+
+// JSX -Javascript xml expression
+//var template = <h1 id="someid">Something new!!</h1>; // Strange assignament
+var template = React.createElement(
+    "h1",
+    { id: "someid" },
+    "Something new!!"
+  );
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
