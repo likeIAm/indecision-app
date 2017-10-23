1f5c99477fe2f345cbff8cfc3bc4263326a8d4eb
olivaluca85@gmail.com <olivaluca85@gmail.com>
2017-10-23 12:28:54 +0200

    Component props

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 class IndecisionApp extends React.Component {
     render () {
+        const title = 'Indecision App';
+        const subTitle = 'Put your life in the hands of a computer';
+        const options = ['1 kjk,', '2 gfdds', '3 kjuk'];
+
         return (
             <div>
-                <Header />
+                <Header title={title} subTitle={subTitle}/>
                 <Action />
-                <Options />
+                <Options options={options}/>
                 <AddOption />
             </div>
         );
@@ -13,10 +17,11 @@ class IndecisionApp extends React.Component {
 
 class Header extends React.Component {
     render() {
+        console.log(this.props);
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of a computer</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subTitle}</h2>
             </div>
         );
     }
@@ -36,8 +41,10 @@ class Options extends React.Component {
     render() {
         return (
             <div>
-                <p> Options component here</p>
-                <Option />
+                {
+                    //this.props.options.map((option) => <p key={option}>{option}</p>)
+                    this.props.options.map((option) => <Option key={option} optionText={option}/>)
+                }
             </div>
         );
     }
@@ -47,7 +54,7 @@ class Option extends React.Component {
     render () {
         return (
             <div>
-                Option
+                Option: {this.props.optionText}
             </div>
         );
     }
