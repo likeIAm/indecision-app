12e94584e4a693f10ac0bca519697d3430d5067f
olivaluca85@gmail.com <olivaluca85@gmail.com>
2017-10-24 16:52:43 +0200

    yarn add react-modal@2.2.2

--- src/app.js
+++ src/app.js
@@ -2,21 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-const Layout = (props) => {
-    return (
-        <div>
-            <p>Header</p>
-            {props.content}
-            {props.children}
-            <p>Footer</p>
-        </div>
-    );
-}
-
-const jsx = <p>First method to pass jsx to a children</p>;
-
-ReactDOM.render(
-    <Layout content={jsx}>
-        <p>Second method to pass jsx to a children</p>
-    </Layout>
-    , document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
