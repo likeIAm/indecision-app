626fdbbf9a08a6d7c28628b6cce037e6040ad88f
olivaluca85@gmail.com <olivaluca85@gmail.com>
2017-10-24 16:49:35 +0200

    Passing children to a component

--- src/app.js
+++ src/app.js
@@ -2,4 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const Layout = (props) => {
+    return (
+        <div>
+            <p>Header</p>
+            {props.content}
+            {props.children}
+            <p>Footer</p>
+        </div>
+    );
+}
+
+const jsx = <p>First method to pass jsx to a children</p>;
+
+ReactDOM.render(
+    <Layout content={jsx}>
+        <p>Second method to pass jsx to a children</p>
+    </Layout>
+    , document.getElementById('app'));
