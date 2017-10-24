8ec57ddfe3bee5583b0402bfe6ee7e4cc3b96eb2
olivaluca85@gmail.com <olivaluca85@gmail.com>
2017-10-24 12:49:43 +0200

    Importing external node modules

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
-import './utils.js'; // like this i simply run that file
-import substracttt, { square, add } from './utils.js'; // The default is not in parenthesis ans i can call like i want
-import isSenior, { isAdult, canDrink } from './person.js';
-console.log('app');
+// Import external node modules
+// install -> import -> use
 
-console.log(square(3));
-console.log(add(3, 5));
-console.log(isAdult(16));
-console.log(substracttt(100, 20));
-console.log('Is Senior: ' + isSenior(20)); 
+import validator from 'validator'; // see that here I'm not using './util' but directly the name of the module, that has an export default
+
+console.log(validator.isEmail('luca@gmail.com'));
